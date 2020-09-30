ef6cb7f15bf14f31639b5165a77d7b151ab1f54e
Abhisheknakka <41500587+Abhisheknakka@users.noreply.github.com>
2020-10-01 00:15:09 +0530

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,12 +1,12 @@
 class Solution(object):
-  def twoSum(self, nums, target):
+  def twoSum(self, n, target):
     """
     :type nums: List[int]
     :type target: int
     :rtype: List[int]
     """
     d = {}
-    for i, num in enumerate(nums):
+    for i, num in enumerate(n):
       if target - num in d:
         return [d[target - num], i]
       d[num] = i
